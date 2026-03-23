7d49e78572a360e65f0ed290b579d5e358f32bb2
Kefas Kingsley <kefasiceking@gmail.com>
2026-03-23 20:32:29 +0100

    fix: prettier error

--- api/src/__tests__/config.test.ts
+++ api/src/__tests__/config.test.ts
@@ -12,7 +12,7 @@ describe('Config Validation', () => {
 
   it('should throw an error if CONTRACT_ID is missing', () => {
     delete process.env.CONTRACT_ID;
-    
+
     expect(() => {
       require('../config/index');
     }).toThrow('CONTRACT_ID environment variable is required');
@@ -20,7 +20,7 @@ describe('Config Validation', () => {
 
   it('should not throw an error if CONTRACT_ID is present', () => {
     process.env.CONTRACT_ID = 'TEST_CONTRACT_ID';
-    
+
     expect(() => {
       require('../config/index');
     }).not.toThrow();
